fb31252832733571811191764e1a6c1ea411d553
RobertoVizz <ligthryuu@gmail.com>
2026-01-09 03:39:04 -0600

    Update plantilla.sql

--- plantilla.sql
+++ plantilla.sql
@@ -1,10 +1,8 @@
--- Crear la tabla de categorías
 CREATE TABLE categorias (
     id_categoria SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
-)
+);
 
--- Crear la tabla de productos
 CREATE TABLE productos (
     id_producto SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL UNIQUE,
@@ -16,18 +14,16 @@ CREATE TABLE productos (
         FOREIGN KEY (id_categoria)
         REFERENCES categorias (id_categoria)
         ON DELETE SET NULL
-)
+);
 
--- Crear la tabla de clientes
 CREATE TABLE clientes (
     id_cliente SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL UNIQUE,
     direccion VARCHAR(200),
     telefono VARCHAR(20)
     
-)
+);
 
--- Crear la tabla de ordenes
 CREATE TABLE ordenes (
     id_orden SERIAL PRIMARY KEY,
     tipo_orden VARCHAR(100) NOT NULL UNIQUE,
